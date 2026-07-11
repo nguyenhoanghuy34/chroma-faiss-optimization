9353223f89bb968358a1b3b858f4644bbd5dc014
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-07-11 23:57:32 +0700

    pdf path

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 # Link data pdf
+LABOR_LAW_PDF_PATH = "D:\Subject\CV2026\Chroma_Faiss\chroma-faiss-optimization\Dataset\labor_law\labor_law.pdf"
+
+import os
+
+BASE_DIR = os.getcwd()
+
+LABOR_LAW_PDF = os.path.join(
+    BASE_DIR,
+    "Dataset",
+    "labor_law",
+    "labor_law.pdf"
+)
